4f28427bf1f60cc71c79a1485844cc5506b27215
horler408 <horler.web@gmail.com>
2022-07-25 04:47:42 +0100

    redux implementation

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-// import store from './store';
-import store from './todo_app/store';
-// import App from './App';
-import App from './todo_app/App';
+import store from './store';
+// import store from './todo_app/store';
+import App from './App';
+// import App from './todo_app/App';
+// import App from './context/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
